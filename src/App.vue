d0483ef993cc5580c4783f5a0c587082d2b7d570
jackhydez <pash.ok92@yandex.ru>
2021-01-20 23:40:37 +0300

    add npm package vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <h1>hello</h1>
+    <!--
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -16,17 +18,20 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  /*
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
   }
+  */
 }
 </script>
 
